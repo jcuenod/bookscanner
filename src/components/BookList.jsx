52ca4c7e3ffcbc32a0e23018d58709abb75142f3
James Cuénod <j3frea+gitkraken@gmail.com>
2019-11-27 15:23:27 -0600

    Add delete button (not wired up)

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -4,19 +4,28 @@ import {
 	ListItem,
 	ListItemText,
 	ListItemAvatar,
-	Avatar
+	Avatar,
+	ListItemSecondaryAction,
+	IconButton
 } from '@material-ui/core'
+import { Delete as DeleteIcon } from '@material-ui/icons'
 
 const BookList = ({ books }) => (
 	<List>
-		{books.map(b => {
+		{books.map(b =>
 			<ListItem>
 				<ListItemAvatar>
 					<Avatar alt={b.title} src={b.imgSrc} />
 				</ListItemAvatar>
 				<ListItemText primary={b.title} secondary={(b.authors || []).join(", ")} />
+
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                 </ListItemSecondaryAction>
 			</ListItem>
-		})}
+		)}
 	</List>
 )
 export default BookList
