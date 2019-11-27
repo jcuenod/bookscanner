56fbdc86e93dcd6c2962b954f0622ea5a3d6e552
James Cuénod <j3frea+gitkraken@gmail.com>
2019-11-27 15:26:28 -0600

    Fix Logic problem with video reset and scanning state in VideoHelper

--- src/util/VideoHelper.js
+++ src/util/VideoHelper.js
@@ -1,8 +1,16 @@
-// import { BrowserQRCodeReader } from '@zxing/library'
 import BooklistManager from './BooklistManager'
 
 let selected_device_id = undefined
-const barcodeReader = new ZXing.BrowserBarcodeReader()
+
+// const barcodeFormatArray = Object.keys(ZXing.BarcodeFormat)
+// const codeHints = ["CODE_128", "EAN_13"].map(code => barcodeFormatArray.findIndex(v => v === code))
+const hints = new Map()
+const formats = [
+	ZXing.BarcodeFormat.CODE_128,
+	ZXing.BarcodeFormat.EAN_13
+]
+hints.set(ZXing.DecodeHintType.POSSIBLE_FORMATS, formats)
+const barcodeReader = new ZXing.BrowserBarcodeReader(500, hints)
 
 barcodeReader.getVideoInputDevices().then((videoInputDevices) => {
 	console.log(videoInputDevices)
@@ -27,6 +35,28 @@ barcodeReader.getVideoInputDevices().then((videoInputDevices) => {
 	// 	console.log(selected_device_id)
 })
 
+const processBookDetails = bookDetails => {
+	if (bookDetails.totalItems > 0) {
+		const title = bookDetails.items[0] ?.volumeInfo ?.title
+		const subtitle = bookDetails.items[0] ?.volumeInfo ?.subtitle
+		const authors = bookDetails.items[0] ?.volumeInfo ?.authors || []
+		const date = bookDetails.items[0] ?.volumeInfo ?.publishedDate.replace(/(.*)(\d{4})(.*)/, '$2')
+		const publisher = bookDetails.items[0] ?.volumeInfo ?.publisher
+		const imgSrc = bookDetails.items[0] ?.volumeInfo ?.imageLinks ?.smallThumbnail
+		const isbn = bookDetails.isbnSearch
+		bookDetails = { title, subtitle, authors, date, publisher, imgSrc, isbn }
+		BooklistManager.add({
+			title,
+			subtitle,
+			authors,
+			date,
+			publisher,
+			imgSrc,
+			isbn
+		})
+	}
+}
+
 const url = (isbn) => `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
 const doIsbnSearch = async (isbn) => {
 	console.log(isbn)
@@ -38,34 +68,37 @@ const doIsbnSearch = async (isbn) => {
 	// })
 }
 
-let was_reset = false
-const scanItem = async () => {
+let is_scanning = false
+const scanItem = async (callback) => {
+	is_scanning = true
 	console.log("scanning!")
 	console.log(selected_device_id)
 	let result = await barcodeReader.decodeOnceFromVideoDevice(selected_device_id, 'video')
-	while (!/\d{13}/.test(result.text) && !was_reset) {
+	while (!/\d{13}/.test(result.text) && is_scanning) {
 		alert("failed to get a real reading (trying to try again)")
 		result = await barcodeReader.decodeOnceFromVideoDevice(selected_device_id, 'video')
 	}
-	if (was_reset) {
-		was_reset = false
+	if (!is_scanning) {
 		return
 	}
 
 	const bookData = await doIsbnSearch(result.text)
 	bookData.isbnSearch = result.text
-	BooklistManager.add(bookData)
-	barcodeReader.reset()
+	processBookDetails(bookData)
+	// barcodeReader.reset()
+	callback()
 	// .catch(err => {
 	// 	console.error(err)
 	// })
 }
-const doReset = () => {
-	was_reset = true
+const reset = () => {
+	is_scanning = false
 	barcodeReader.reset()
+	barcodeReader.hints = hints
+	console.log("Reset barcode reader")
 }
 const VideoHelper = {
-	scanItem: params => scanItem(params),
-	reset: doReset
+	scanItem,
+	reset
 }
 export default VideoHelper
